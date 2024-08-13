2ddc2a79b8c8a735d161c129cf4dc0f4db9fc159
Owen <owenfriedman07@gmail.com>
2024-08-13 12:07:07 -0400

    modified for srx. subsystems coming soon

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,41 +4,50 @@
 
 package frc.robot;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_robotDrive;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  
+  private double m_leftStick;
+  private double m_rightStick;
+  private final TalonSRX leftmotor=new TalonSRX(4);
+  private final TalonSRX leftmotorfollow=new TalonSRX(5);
+  private final TalonSRX rightmotor=new TalonSRX(6);
+  private final TalonSRX rightmotorfollow=new TalonSRX(7);
+  PS4Controller controller=new PS4Controller(0);
+ 
 
   @Override
   public void robotInit() {
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
-
+    rightmotor.setInverted(true);
+    rightmotorfollow.setInverted(true);
+    rightmotorfollow.follow(rightmotor);
+    leftmotorfollow.follow(leftmotor);
+    
+    
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    
 
-    m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    
+    m_leftStick =-controller.getLeftY();
+    m_rightStick = -controller.getRightY();
   }
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    
+    leftmotor.set(TalonSRXControlMode.PercentOutput,m_leftStick);
+    rightmotor.set(TalonSRXControlMode.PercentOutput,m_rightStick);
+  
   }
 }
